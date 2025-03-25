a925d7c00e8a53cb5fdf1f0fd792ccd26333c651
Mateus Resende Ottoni <mateus.resende.ottoni@gmail.com>
2025-03-25 19:27:39 -0300

    Patch DesafioHacka07
    Finalização e expansão da lista de músicas e artistas e alteração da paleta de cores

--- DesafioHacka07/ContentView.swift
+++ DesafioHacka07/ContentView.swift
@@ -31,8 +31,8 @@ struct Singer: Identifiable {
 struct ContentView: View {
 
     var paletaDeCores : ColorPalette =
-    ColorPalette(background1: Color(red: 0.2, green: 0.10, blue: 0.95),
-                 background2: Color(red: 0.1, green: 0.01, blue: 0.2),
+    ColorPalette(background1: Color(red: 0.80, green: 0.10, blue: 0.20),
+                 background2: Color(red: 0.15, green: 0.0, blue: 0.1),
                  text: Color.white,
                  details: Color.white)
     
@@ -42,34 +42,44 @@ struct ContentView: View {
         Song(id: 3, name: "Faces da Morte", artist: "TakaB", year: 2023, cover: "https://i.scdn.co/image/ab67616d0000b273004bc9fce575840e69d148d2"),
         Song(id: 4, name: "Universo 25", artist: "TakaB", year: 2023, cover: "https://i.scdn.co/image/ab67616d0000b2738b1aef18c0f536d673d39abf"),
         Song(id: 5, name: "Querido Demônio", artist: "TakaB", year: 2023, cover: "https://i.scdn.co/image/ab67616d0000b273a7430c1e7f671f22e5e7cae8"),
-        Song(id: 6, name: "Tempo de Caos", artist: "TakaB", year: 2023, cover: "Capa"),
-        Song(id: 7, name: "Lamentos de um Falcão", artist: "TakaB", year: 2024, cover: "https://i.scdn.co/image/ab67616d0000b273cf1b2b34df1de1144ed4614b"),
+        Song(id: 6, name: "Tempo de Caos", artist: "TakaB", year: 2023, cover: "https://i.scdn.co/image/ab67616d0000b27394c146b55cc65720f0230eae"),
+        Song(id: 7, name: "Lamentos de um Falcão", artist: "TakaB", year: 2024, cover: "https://i.scdn.co/image/ab67616d0000b273611dc2ea0a7e85643c406dbc"),
         Song(id: 8, name: "Rei dos Pesadelos", artist: "TakaB", year: 2024, cover: "https://i.scdn.co/image/ab67616d0000b2737496ddd694fa337981e14bb9"),
-        Song(id: 9, name: "Do Lado de Cá", artist: "TakaB", year: 2024, cover: "Capa"),
-        Song(id: 10, name: "Sete Mares", artist: "TakaB", year: 2024, cover: "https://i.scdn.co/image/ab67616d0000b27394c146b55cc65720f0230eae"),
+        Song(id: 9, name: "Do Lado de Cá", artist: "TakaB", year: 2024, cover: "https://i.scdn.co/image/ab67616d0000b273b600a93fcd30fa57ef27a5eb"),
+        Song(id: 10, name: "Sete Mares", artist: "TakaB", year: 2024, cover: "https://i.scdn.co/image/ab67616d0000b273cf1b2b34df1de1144ed4614b"),
         Song(id: 11, name: "Meu Querido Rei", artist: "TakaB", year: 2024, cover: "https://i.scdn.co/image/ab67616d0000b27352f7625a9de746bfcc87af43"),
         Song(id: 12, name: "Cicatrizes do Tempo", artist: "Takab", year: 2024, cover: "https://i.scdn.co/image/ab67616d0000b2733e56a54984271c3cee290863"),
-        Song(id: 13, name: "Dever de Consumação", artist: "RafãoMusic", year: 2024, cover: "Capa"),
-        Song(id: 14, name: "Tempo", artist: "RafãoMusic", year: 2024, cover: "Capa"),
-        Song(id: 15, name: "Réquiem Carmesim", artist: "RafãoMusic", year: 2025, cover: "Capa"),
-        Song(id: 16, name: "Joker", artist: "RafãoMusic", year: 2025, cover: "Capa"),
-        Song(id: 17, name: "Razão do Vazio", artist: "RafãoMusic", year: 2025, cover: "Capa"),
-        Song(id: 18, name: "Juramento Vingativo", artist: "RafãoMusic", year: 2025, cover: "Capa2"),
+        Song(id: 13, name: "Dever de Consumação", artist: "RafãoMusic", year: 2024, cover: "https://i.scdn.co/image/ab67616d0000b273e363e8659c867c37c8271942"),
+        Song(id: 14, name: "Tempo", artist: "RafãoMusic", year: 2024, cover: "https://i.scdn.co/image/ab67616d0000b2730e363b06e4ca848b23d3462b"),
+        Song(id: 15, name: "Réquiem Carmesim", artist: "RafãoMusic", year: 2025, cover: "https://i.scdn.co/image/ab67616d0000b273ffc624a5197e98fb34b15452"),
+        Song(id: 16, name: "Joker", artist: "RafãoMusic", year: 2025, cover: "https://i.scdn.co/image/ab67616d0000b273628645cb059e729a5d48dc2c"),
+        Song(id: 17, name: "Razão do Vazio", artist: "RafãoMusic", year: 2025, cover: "https://i.scdn.co/image/ab67616d0000b273fa2d772e877da268ce5eab36"),
+        Song(id: 18, name: "Juramento Vingativo", artist: "RafãoMusic", year: 2025, cover: "https://i.scdn.co/image/ab67616d0000b2731a69a4be6e9ec0c85ff7a936"),
         Song(id: 19, name: "Goodnight", artist: "oShaman", year: 2023, cover: "https://i.scdn.co/image/ab67616d0000b273a8789d438d8d5d3989cc7f62"),
         Song(id: 20, name: "Solidão", artist: "oShaman", year: 2023, cover: "https://i.scdn.co/image/ab67616d0000b2731821dbe2af4766865f8e3aa1"),
         Song(id: 21, name: "Crescer", artist: "oShaman", year: 2023, cover: "https://i.scdn.co/image/ab67616d0000b2735ab95342190bd78bc59f724a"),
         Song(id: 22, name: "Mudar", artist: "oShaman", year: 2023, cover: "https://i.scdn.co/image/ab67616d0000b2734a260bf80a3cea35f042eafe"),
         Song(id: 23, name: "Deriva", artist: "oShaman", year: 2024, cover: "https://i.scdn.co/image/ab67616d0000b273100edafde41a48fc2d1365ca"),
-        Song(id: 24, name: "Nasci pra Atirar", artist: "oShaman", year: 2024, cover: "https://i.scdn.co/image/ab67616d0000b27392ac336c9bfe806771b404c7")
+        Song(id: 24, name: "Nasci pra Atirar", artist: "oShaman", year: 2024, cover: "https://i.scdn.co/image/ab67616d0000b27392ac336c9bfe806771b404c7"),
+        Song(id: 25, name: "Chamas Negras Mortais", artist: "Enygma", year: 2022, cover: "https://i.scdn.co/image/ab67616d0000b2735e0ae7e857a411f47778998d"),
+        Song(id: 26, name: "Caçador de Recompensas", artist: "Enygma", year: 2022, cover: "https://i.scdn.co/image/ab67616d0000b273452a12707717beeaaded20c2"),
+        Song(id: 27, name: "Terror em Londres", artist: "Enygma", year: 2022, cover: "https://i.scdn.co/image/ab67616d0000b27318464a8d3bb772c3132c79e1"),
+        Song(id: 28, name: "Hades", artist: "Enygma", year: 2022, cover: "https://i.scdn.co/image/ab67616d0000b273c53167dc04801e64df976cea"),
+        Song(id: 29, name: "Mágico Assassino", artist: "Enygma", year: 2022, cover: "https://i.scdn.co/image/ab67616d0000b27306b5679b8131aca0437ba487"),
+        Song(id: 30, name: "Frio", artist: "Enygma", year: 2022, cover: "https://i.scdn.co/image/ab67616d0000b273ae0235fbe32669b6a38744f0")
     ]
     
     var arrayArtistas = [
-    Singer(id: 1, name: "TakaB", photo: "https://i.scdn.co/image/ab67616100005174d3c465a866c51048a3048c52"),
-    Singer(id: 2, name: "Chrono", photo: "https://i.scdn.co/image/ab6761610000e5eb3e9f7b144bbf065cdfceeec8"),
-    Singer(id: 3, name: "Novatroop", photo: "https://yt3.googleusercontent.com/F3oki5DQBKxMmYFcI7p6gAebpY1Eed9eC5L3We-Z-Dj4m8oQ4Q0HPn86H3cAlBeM8LSLlU1jRWc=s900-c-k-c0x00ffffff-no-rj"),
-    Singer(id: 4, name: "Henrique Mendonça", photo: "https://yt3.googleusercontent.com/1bD4CHZOf3Q1cz-2L9ElINzqoHVIo8mYbhrB27G-tBKO9uV5JahzxEbpvL-lJXwvoMwa90w=s176-c-k-c0x00ffffff-no-rj-mo"),
-    Singer(id: 5, name: "oShaman", photo: "https://yt3.googleusercontent.com/GqZW4T0sVeiyJSwAG2S7KiXIbgWGuvwAdumH1IbaRv-gUjWKTf8bgndwpsEFXpDWSj6oBL0F04g=s900-c-k-c0x00ffffff-no-rj"),
-    Singer(id: 6, name: "Anny", photo: "https://yt3.googleusercontent.com/U0sgzPJjII71XVEuUA3niwegOAmZ0CK-rwBtk4Q1S2ZleYoTUGJ6dvyBa4FQhKCvlD2npwDjEvw=s900-c-k-c0x00ffffff-no-rj")
+    Singer(id: 1, name: "Rodrigo Zin", photo: "https://yt3.googleusercontent.com/5TZ08juI7iVIkU7B1ayFOHDR6tAX5fpB8TOWlkqfRimgJ0WEGHroIOGpjyV-NeVDwL2AUyeV=s160-c-k-c0x00ffffff-no-rj"),
+    Singer(id: 2, name: "TakaB", photo: "https://i.scdn.co/image/ab67616100005174d3c465a866c51048a3048c52"),
+    Singer(id: 3, name: "Chrono", photo: "https://i.scdn.co/image/ab6761610000e5eb3e9f7b144bbf065cdfceeec8"),
+    Singer(id: 4, name: "Novatroop", photo: "https://yt3.googleusercontent.com/F3oki5DQBKxMmYFcI7p6gAebpY1Eed9eC5L3We-Z-Dj4m8oQ4Q0HPn86H3cAlBeM8LSLlU1jRWc=s900-c-k-c0x00ffffff-no-rj"),
+    Singer(id: 5, name: "Henrique Mendonça", photo: "https://yt3.googleusercontent.com/1bD4CHZOf3Q1cz-2L9ElINzqoHVIo8mYbhrB27G-tBKO9uV5JahzxEbpvL-lJXwvoMwa90w=s176-c-k-c0x00ffffff-no-rj-mo"),
+    Singer(id: 6, name: "oShaman", photo: "https://yt3.googleusercontent.com/GqZW4T0sVeiyJSwAG2S7KiXIbgWGuvwAdumH1IbaRv-gUjWKTf8bgndwpsEFXpDWSj6oBL0F04g=s900-c-k-c0x00ffffff-no-rj"),
+    Singer(id: 7, name: "Anny", photo: "https://yt3.googleusercontent.com/U0sgzPJjII71XVEuUA3niwegOAmZ0CK-rwBtk4Q1S2ZleYoTUGJ6dvyBa4FQhKCvlD2npwDjEvw=s900-c-k-c0x00ffffff-no-rj"),
+    Singer(id: 8, name: "Enygma", photo: "https://i.scdn.co/image/ab6761610000e5eb6ab81cf8cf869ce14a31fade"),
+    Singer(id: 9, name: "RafãoMusic", photo: "https://yt3.googleusercontent.com/DAegCC9w2OMdQAoX7PVciE_gQLC6I6ScRAzcoXG5AUqXOxpAD405hFpXXvcYZvEpI6Z1_WS4Fw=s900-c-k-c0x00ffffff-no-rj"),
+    Singer(id: 10, name: "VMZ", photo: "https://yt3.googleusercontent.com/uMN1cJrCTrxqooWElNv5LRQlPpLpK_p9QoYzJpOCxdUdbcQSA2lRkttVWvLfT66x1FiGnwoO9Q=s900-c-k-c0x00ffffff-no-rj")
     ]
     
     var body: some View {
@@ -85,8 +95,9 @@ struct ContentView: View {
                     ScrollView {
                         Image("AlbumCover")  // Capa do álbum
                             .resizable()
-                            .frame(width: 130, height: 180)
+                            .frame(width: 189, height: 204)
                             .shadow(color: paletaDeCores.details,radius: 2)
+                            .padding()
                         Text("MateusFM")         // Nome do álbum
                             .frame(maxWidth: .infinity, alignment: .leading)
                             .foregroundStyle(paletaDeCores.text)
@@ -94,9 +105,13 @@ struct ContentView: View {
                             .font(.title)
                             .padding(.leading, 10)
                         HStack {
-                            Image("AlbumOwner")   // Criador do álbum
-                                .resizable()
-                                .frame(width: 50, height: 50)
+                            AsyncImage (url: URL(string: "https://avatars.githubusercontent.com/u/161080996?v=4")) { image in
+                                image.resizable()
+                            } placeholder: {
+                                ProgressView()
+                                    .shadow(color: paletaDeCores.details ,radius: 1)
+                            }
+                            .frame(width: 45, height: 45)
                             Text("Mateus Ottoni")           // Nome do criador
                                 .foregroundStyle(paletaDeCores.text)
                             Spacer()
@@ -127,6 +142,7 @@ struct ContentView: View {
                                 } label: {
                                     Image(systemName: "ellipsis")
                                         .foregroundStyle(paletaDeCores.details)
+                                        .shadow(color: paletaDeCores.details, radius: 5)
                                 }
                             }
                             .padding([.trailing, .leading], 15)
--- DesafioHacka07/MusicView.swift
+++ DesafioHacka07/MusicView.swift
@@ -12,8 +12,8 @@ struct MusicView: View {
     @State var musica : Song
     
     var paletaDeCores : ColorPalette =
-    ColorPalette(background1: Color(red: 0.2, green: 0.10, blue: 0.95),
-                 background2: Color(red: 0.1, green: 0.01, blue: 0.45),
+    ColorPalette(background1: Color(red: 0.80, green: 0.10, blue: 0.20),
+                 background2: Color(red: 0.05, green: 0.0, blue: 0.1),
                  text: Color.white,
                  details: Color.white)
     
@@ -46,24 +46,49 @@ struct MusicView: View {
                 HStack {
                     Image(systemName: "shuffle")
                         .font(.custom("ComicSans", size: 30))
-                        .foregroundStyle(paletaDeCores.details)
+                        .foregroundStyle(.black)
+                        .shadow(color: paletaDeCores.details, radius: 1)
+                        .shadow(color: paletaDeCores.details, radius: 1)
+                        .shadow(color: paletaDeCores.details, radius: 1)
+                        .shadow(color: paletaDeCores.details, radius: 2)
+                        .shadow(color: paletaDeCores.details, radius: 2)
                     Spacer()
                     Image(systemName: "backward.end.fill")
                         .font(.custom("ComicSans", size: 40))
-                        .foregroundStyle(paletaDeCores.details)
+                        .foregroundStyle(.black)
+                        .shadow(color: paletaDeCores.details, radius: 1)
+                        .shadow(color: paletaDeCores.details, radius: 1)
+                        .shadow(color: paletaDeCores.details, radius: 1)
+                        .shadow(color: paletaDeCores.details, radius: 2)
+                        .shadow(color: paletaDeCores.details, radius: 2)
                     Spacer()
                     Image(systemName: "play.fill")
                         .font(.custom("ComicSans", size: 60))
                         .fontWeight(.bold)
-                        .foregroundStyle(paletaDeCores.details)
+                        .foregroundStyle(.black)
+                        .shadow(color: paletaDeCores.details, radius: 1)
+                        .shadow(color: paletaDeCores.details, radius: 1)
+                        .shadow(color: paletaDeCores.details, radius: 1)
+                        .shadow(color: paletaDeCores.details, radius: 2)
+                        .shadow(color: paletaDeCores.details, radius: 2)
                     Spacer()
                     Image(systemName: "forward.end.fill")
                         .font(.custom("ComicSans", size: 40))
-                        .foregroundStyle(paletaDeCores.details)
+                        .foregroundStyle(.black)
+                        .shadow(color: paletaDeCores.details, radius: 1)
+                        .shadow(color: paletaDeCores.details, radius: 1)
+                        .shadow(color: paletaDeCores.details, radius: 1)
+                        .shadow(color: paletaDeCores.details, radius: 2)
+                        .shadow(color: paletaDeCores.details, radius: 2)
                     Spacer()
                     Image(systemName: "repeat")
                         .font(.custom("ComicSans", size: 30))
-                        .foregroundStyle(paletaDeCores.details)
+                        .foregroundStyle(.black)
+                        .shadow(color: paletaDeCores.details, radius: 1)
+                        .shadow(color: paletaDeCores.details, radius: 1)
+                        .shadow(color: paletaDeCores.details, radius: 1)
+                        .shadow(color: paletaDeCores.details, radius: 2)
+                        .shadow(color: paletaDeCores.details, radius: 2)
                 }
                 .padding([.trailing, .leading], 10)
                 Spacer()
@@ -73,5 +98,5 @@ struct MusicView: View {
 }
 
 #Preview {
-    MusicView(musica: Song(id: 5, name: "Teste", artist: "Testudo", year: 2001, cover: "Capa"))
+    MusicView(musica: Song(id: 5, name: "Teste", artist: "Testudo", year: 2001, cover: "https://i.scdn.co/image/ab67616d0000b273af6662633178b2e1d4a206f6"))
 }
